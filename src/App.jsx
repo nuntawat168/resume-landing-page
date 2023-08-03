379f30b0a96d3a040072e8dd74410b47ec724f34
Ice Nantawat <nuntawat_168@outlook.com>
2023-08-03 13:00:29 +0700

    Set up Vite + React project structure
    - Created src folder with components and styles
    - Added initial landing page components
    - Configured Vite build and development settings

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import Content from "./components/content";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
